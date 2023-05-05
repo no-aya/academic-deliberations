237fd982edb09831d86226f7a8ad4946066adca4
Aya <aya.aitoulahyane@gmail.com>
2023-05-05 18:05:44 +0100

    Initiating the BE

---
+++ delibrations/src/main/java/ma/enset/delibrations/DelibrationsApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.delibrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelibrationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelibrationsApplication.class, args);
+    }
+
+}
