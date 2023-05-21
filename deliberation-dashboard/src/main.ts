038c5bdd0a5191b6116d57c5a4b3388646d87c30
Aya <aya.aitoulahyane@gmail.com>
2023-05-21 16:15:41 +0100

    Refractoring & indexing

--- deliberation-dashboard/src/main.ts
+++ deliberation-dashboard/src/main.ts
@@ -3,17 +3,6 @@
 =========================================================
 * Deliberation Dashboard Angular - v1.5.0
 =========================================================
-
-* Product Page: https://www.enset-media.ac.ma/product/deliberation-dashboard
-* Copyright 2023 no.aya (https://www.enset-media.ac.ma)
-* Licensed under MIT (https://github.com/no.aya/deliberation-dashboard/blob/master/LICENSE.md)
-
-* Coded by no.aya
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
