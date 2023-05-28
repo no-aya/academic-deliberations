798f93666a541dbd875bc7128405b36f723fb59d
asmaeilloussamen <a.illoussamen@etu.enset-media.ac.ma>
2023-05-28 09:42:31 +0100

    Get users test works

--- deliberation-dashboard/src/app/pages/comptes/comptes.component.ts
+++ deliberation-dashboard/src/app/pages/comptes/comptes.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {User} from "../model/user.model";
+import {FormGroup} from "@angular/forms";
+import {ComptesService} from "../../services/comptes.service";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-comptes',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComptesComponent implements OnInit {
 
-  constructor() { }
+  users!: any;
+  errorMessage!: string;
+  searchFormGroup : FormGroup | undefined;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get("http://localhost:8089/users").subscribe(data=>{
+      this.users=data;
+      },error=>{
+        console.log(error);
+      })
+
   }
 
 }
--- deliberation-dashboard/src/app/services/comptes.service.ts
+++ deliberation-dashboard/src/app/services/comptes.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../pages/model/user.model";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComptesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getComptes():Observable<Array<User>>{
+    return this.http.get<Array<User>>(environment.backendHost+"/users");
+  }
 }
