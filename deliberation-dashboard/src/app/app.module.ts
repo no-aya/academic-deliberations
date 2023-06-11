db884947255eaa00c2100a36ec252a183c358362
asmaeilloussamen <a.illoussamen@etu.enset-media.ac.ma>
2023-06-11 13:03:21 +0100

    Prof add succes

--- deliberation-dashboard/src/app/app.module.ts
+++ deliberation-dashboard/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { ProfEtudComponent } from './pages/prof-etud/prof-etud.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { ComponentsModule } from './components/components.module';
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    ProfEtudComponent
 
   ],
   providers: [],
