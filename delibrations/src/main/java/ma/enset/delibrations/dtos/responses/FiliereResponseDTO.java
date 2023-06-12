3e7c8b52412e947204fa5860f7f0bd0469d9753d
asmaeilloussamen <a.illoussamen@etu.enset-media.ac.ma>
2023-06-12 18:39:19 +0100

    Unit Test Filiere

--- delibrations/src/main/java/ma/enset/delibrations/dtos/responses/FiliereResponseDTO.java
+++ delibrations/src/main/java/ma/enset/delibrations/dtos/responses/FiliereResponseDTO.java
@@ -1,6 +1,7 @@
 package ma.enset.delibrations.dtos.responses;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class FiliereResponseDTO {
     private Long id;
     private String code;
